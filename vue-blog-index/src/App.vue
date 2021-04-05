004c07e11f38d213d431a6900113b102044fed8e
ivanjo39191 <ivanjo39191@gmail.com>
2021-04-05 16:11:50 +0000

    refactor: 調整 App.vue 入口 說明: 1. 移除導航列至 navbar

--- vue-blog-index/src/App.vue
+++ vue-blog-index/src/App.vue
@@ -1,22 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <transition name="fade">
-      <keep-alive>
-      <div>
-        <router-link to="/index">Home</router-link><br>
-        <router-link to="/index/home">index/Home</router-link><br>
-        <router-link to="/profile/index">profile</router-link><br>
-        <router-link to="/aprofile/index">aprofile</router-link><br>
-        <router-link to="/profile/hello">pofilehello</router-link><br>
-        <router-link to="/test1">test1</router-link><br>
-        <router-view></router-view>
-        <el-dropdown-item divided @click.native="logout">
-          <span style="display:block;">Log Out</span>
-        </el-dropdown-item>
-      </div>
-      </keep-alive>
-    </transition>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -24,15 +8,6 @@
 
 export default {
   name: 'App',
-  methods: {
-    toggleSideBar() {
-      this.$store.dispatch('app/toggleSideBar')
-    },
-    async logout() {
-      await this.$store.dispatch('user/logout')
-      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
-    }
-  }
 }
 </script>
 
@@ -43,6 +18,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
