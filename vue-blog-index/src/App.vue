82a1092e7c66072cf8f7bd4ebf1a6cb079f97f12
ivanjo39191 <ivanjo39191@gmail.com>
2021-04-10 09:41:45 +0000

    build: 添加 google font 'Noto Sans TC' 說明: 1. 調整網站字體
    調整項目:
    1. src/App.vue style

--- vue-blog-index/src/App.vue
+++ vue-blog-index/src/App.vue
@@ -11,9 +11,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url(//fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap);
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans TC', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
