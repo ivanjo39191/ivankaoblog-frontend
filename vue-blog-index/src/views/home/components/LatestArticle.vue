e937a9b4b34fbe3526f35d63a275d3ef378691a6
ivanjo39191 <ivanjo39191@gmail.com>
2021-06-27 04:14:15 +0000

    feat: 添加首頁最新文章區塊圖片 說明: 1. 最新文章第一篇添加圖片
    調整項目:
    1. vue-blog-index/src/views/home/components/LatestArticle.vue template 添加 article-banner img
    2. vue-blog-index/src/views/home/components/LatestArticle.vue js 添加 backendUrl
    3. vue-blog-index/src/views/home/components/LatestArticle.vue css 添加 .article-banner, .article-banner > img

--- vue-blog-index/src/views/home/components/LatestArticle.vue
+++ vue-blog-index/src/views/home/components/LatestArticle.vue
@@ -6,10 +6,13 @@
     <div class="col-md-6 col-xs-12">
       <div class="article" v-bind:key="article.id" v-for="article in articles.slice(0, 1)">
         <p class="title-first">{{ article.title }}</p>
+        <div class="article-banner">
+          <img v-bind:src="backendUrl + article.banner"/>
+        </div>
       </div>
     </div>
     <div class="col-md-6 col-xs-12">
-      <div class="article" v-bind:key="article.id" v-for="article in articles.slice(1, 6)">
+      <div class="article" v-bind:key="article.id" v-for="article in articles.slice(1, 8)">
         <p calss="title">{{ article.title }}</p>
       </div>
     </div>
@@ -28,6 +31,7 @@ export default {
   data(){
     return{
         articles : [],
+        backendUrl : process.env.VUE_APP_BACKEND_SERVER,
     }
   },
   methods: {
@@ -80,4 +84,18 @@ export default {
   font-size: 12px;
 }
 
+.article-banner {
+  display: flex;
+  justify-content: center;
+  height: 15rem;
+  width: 20rem;
+}
+
+.article-banner > img {
+  display: flex;
+  justify-self: center;
+  max-height: 100%;
+  max-width: 100%;
+  overflow: hidden;
+}
 </style>
