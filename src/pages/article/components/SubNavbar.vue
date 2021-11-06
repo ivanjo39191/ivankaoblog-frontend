85bc77d3bf172caf9877926068e44d1914ee3973
ivanjo39191 <ivanjo39191@gmail.com>
2021-11-06 11:58:16 +0000

    feat: add article detail page description: 1. add article page index 2. add article detail v-html and style 3. add article subnavbar

---
+++ src/pages/article/components/SubNavbar.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="subnavbar-main">
+    <div class="subnavbar-items">
+      <router-link to="/">
+        <div class="subnavbar-title">
+          {{ homeCarouselItem.title }}
+        </div>
+      </router-link>
+      <button v-for="item in blogTypeItems" :key="item.id" class="subnavbar-tab">
+        {{ item.type_name }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getHomeCarousel, getBlogType } from '@/api/blog'
+
+export default {
+  name: 'SubNavbar',
+  data () {
+    return {
+      appTitle: 'SubNavbar',
+      sidebar: false,
+      homeCarouselItem: [],
+      blogTypeItems: []
+    }
+  },
+  mounted () {
+    this.getBlogType()
+    this.getHomeCarousel()
+  },
+  methods: {
+    getHomeCarousel () {
+      return new Promise((resolve, reject) => {
+        getHomeCarousel()
+          .then((response) => {
+            // console.log(JSON.stringify(response))
+            this.homeCarouselItem = response[0]
+            resolve()
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      })
+    },
+    getBlogType () {
+      return new Promise((resolve, reject) => {
+        getBlogType()
+          .then((response) => {
+            // console.log(JSON.stringify(response))
+            this.blogTypeItems = response
+            resolve()
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      })
+    }
+  }
+}
+</script>
+<style>
+.subnavbar-main {
+  display: flex;
+  align-items: center;
+  background-color: #fff;
+  height: 8vh;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  margin-bottom: 3vh;
+}
+
+.subnavbar-items {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding-left: 15vw;
+  padding-right: 15vw;
+}
+
+.subnavbar-items > a {
+  text-decoration: none;
+}
+
+.subnavbar-title {
+  font-size: 1.25rem;
+  font-weight: 300;
+  color: #000;
+}
+
+.subnavbar-tab {
+  font-size: 1rem;
+  color: rgba(0, 0, 0, 0.54);
+  margin-left: 1vw;
+  margin-right: 1vw;
+}
+
+</style>
---
+++ src/pages/article/index.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="article">
+    <div class="article-subnavbar">
+      <SubNavbar />
+    </div>
+    <div class="main">
+      <div class="article-content">
+        <h1 class="title">
+          {{ article.title }}
+        </h1>
+        <div class="subtitle">
+          {{ article.subtitle }}
+        </div>
+        <div class="image">
+          <img :src="article.banner">
+        </div>
+        <div class="content" v-html="article.content" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getArticleDetail } from '@/api/blog'
+// import HomeCarousel from './home/components/HomeCarousel'
+import SubNavbar from './components/SubNavbar'
+// import Latest from './home/components/Latest'
+export default {
+  name: 'Article',
+  components: {
+    // HomeCarousel,
+    SubNavbar
+    // Latest
+  },
+  layout: 'index',
+  data () {
+    return {
+      article: [],
+      backendUrl: process.env.VUE_APP_BACKEND_SERVER
+    }
+  },
+  mounted () {
+    this.getArticleDetail(this.$route.query.id, this.$route.query.title)
+  },
+  methods: {
+    getArticleDetail (id, title) {
+      return new Promise((resolve, reject) => {
+        getArticleDetail(id, title)
+          .then((response) => {
+            // console.log(JSON.stringify(response))
+            this.article = response
+            console.log(JSON.stringify(this.article))
+            resolve()
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      })
+    }
+  }
+}
+
+</script>
+<style scoped>
+.main {
+  padding-left: 32vw;
+  padding-right: 32vw;
+}
+
+.image > img {
+  display: flex;
+  width: 36vw;
+}
+
+.article-content >>> h1 {
+  color: #292929;
+  font-family: 'Noto Sans TC', sans-serif;
+  font-size: 1.875rem;
+  font-weight: 300;
+  letter-spacing: 0.04rem;
+  line-height: 1.5;
+  margin: 1.5rem 0;
+}
+
+.article-content >>> h2 {
+  color: #292929;
+  font-size: 2rem;
+}
+
+.article-content >>> h3 {
+  color: #292929;
+  font-size: 1.5rem;
+}
+
+.article-content >>> h4 {
+  color: #292929;
+}
+
+.article-content >>> p {
+  color: #292929;
+  font-size: 1.3125rem;
+  font-weight: 300;
+  letter-spacing: 0.04rem;
+  line-height: 1.5;
+  margin: 2rem 0;
+}
+
+.article-content >>> blockquote {
+  display: block;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 40px;
+  margin-inline-end: 40px;
+}
+
+.article-content >>> blockquote > p {
+  color: rgba(117, 117, 117, 1);
+  letter-spacing: -0.009em;
+  line-height: 2.5rem;
+  font-weight: 300;
+  font-size: 1.75rem;
+  margin-top: 0.75rem;
+  margin-bottom: -0.46rem;
+  display: block;
+  margin-block-start: 1rem;
+  margin-block-end: 1rem;
+  margin-inline-start: 0;
+  margin-inline-end: 0;
+}
+
+.article-content >>> hr {
+  margin-left: 17vw;
+  width: 27px;
+  border: 5px dotted #292929;
+  border-style: none none dotted;
+  color: #fff;
+  background-color: #fff;
+}
+</style>
