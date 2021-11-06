f408c51120be282970a4f2cd777b7d99d5569faa
ivanjo39191 <ivanjo39191@gmail.com>
2021-11-06 11:59:23 +0000

    feat: add blog API description: 1. add getBlogTypeTitle 2. add getBlogSubTypeTitle 3. add getArticleDetail 4. add getHomeCarousel 5. add getBlogType

--- src/api/blog.js
+++ src/api/blog.js
@@ -1,29 +1,50 @@
 import request from '@/utils/request'
 
-export function getBlog() {
+export function getBlog () {
   return request({
     url: process.env.VUE_APP_BACKEND_SERVER + '/api/blog/blog/',
-    method: 'get',
+    method: 'get'
   })
 }
 
-export function getBlogTitle() {
+export function getBlogTitle () {
   return request({
     url: process.env.VUE_APP_BACKEND_SERVER + '/api/blog/blog/title/',
-    method: 'get',
+    method: 'get'
   })
 }
 
-export function getBlogTypeTitle(type, subtype) {
+export function getBlogTypeTitle (type) {
+  return request({
+    url: process.env.VUE_APP_BACKEND_SERVER + '/api/blog/blog/blogtype/?types=' + type,
+    method: 'get'
+  })
+}
+
+export function getBlogSubTypeTitle (type, subtype) {
   return request({
     url: process.env.VUE_APP_BACKEND_SERVER + '/api/blog/blog/blogtype/?types=' + type + '&subtypes' + subtype,
-    method: 'get',
+    method: 'get'
   })
 }
 
-export function getArticleDetail(id) {
+export function getArticleDetail (id, title) {
   return request({
-    url: process.env.VUE_APP_BACKEND_SERVER + '/api/blog/blog/' + id,
-    method: 'get',
+    url: process.env.VUE_APP_BACKEND_SERVER + '/api/blog/blog/' + id + '/?title' + title,
+    method: 'get'
+  })
+}
+
+export function getHomeCarousel () {
+  return request({
+    url: process.env.VUE_APP_BACKEND_SERVER + '/api/blog/homecarousel/',
+    method: 'get'
+  })
+}
+
+export function getBlogType () {
+  return request({
+    url: process.env.VUE_APP_BACKEND_SERVER + '/api/blog/blogtype/',
+    method: 'get'
   })
 }
