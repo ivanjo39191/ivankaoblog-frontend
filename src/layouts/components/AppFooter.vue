bf199b88264915861b85cc80324c30933262fbbc
ivanjo39191 <ivanjo39191@gmail.com>
2022-02-04 03:36:32 +0000

    refactor: 調整 footer css

--- src/layouts/components/AppFooter.vue
+++ src/layouts/components/AppFooter.vue
@@ -61,97 +61,150 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.app-footer {
-  display: grid;
-  grid-template-columns: 16.66vw 66.6vw 16.66vw;
-  background-color: #fff;
-  height: 27.7vh;
-  width: 100vw;
+@media (max-width: 750px) {
+  .app-footer {
+    display: flex;
+    background-color: #fff;
+    width: 100vw;
+  }
+
+  .left {
+    display: flex;
+    width: 80vw;
+    margin: 5vw;
+    color: #000;
+    font-size: 1.25rem;
+    font-weight: 300;
+    letter-spacing: 0.05em;
+  }
+
+  .right {
+    display: flex;
+    flex-flow: wrap;
+    width: 80vw;
+    margin: 5vw;
+  }
+
+  .right-obj {
+    display: flex;
+    flex-flow: wrap;
+    width: 80vw;
+    margin: 1vw;
+  }
+
+  .right-obj-title {
+    width: 80vw;
+    color: #000;
+    font-size: 1.5rem;
+    font-weight: 500;
+    letter-spacing: 0.05em;
+    text-align: left;
+  }
+
+  .right-obj-content > p {
+    width: 80vw;
+    color: #000;
+    font-size: 1.125rem;
+    font-weight: 300;
+    letter-spacing: 0.05em;
+    text-align: left;
+    margin: 0.5rem 0 0 0;
+  }
 }
 
-.main {
-  grid-column: 2 / 3;
-  display: grid;
-  grid-template-columns: 26vw 40.6vw;
-  height: 27.7vh;
-}
+@media (min-width: 750px) {
+  .app-footer {
+    display: grid;
+    grid-template-columns: 16.66vw 66.6vw 16.66vw;
+    background-color: #fff;
+    height: 27.7vh;
+    width: 100vw;
+  }
 
-.left {
-  grid-column: 1 / 2;
-  width: 26vw;
-}
+  .main {
+    grid-column: 2 / 3;
+    display: grid;
+    grid-template-columns: 26vw 40.6vw;
+    height: 27.7vh;
+  }
 
-.left-top {
-  display: grid;
-  grid-template-rows: 15% 4.0785rem 15% auto;
-  height: 27.7vh;
-}
+  .left {
+    grid-column: 1 / 2;
+    width: 26vw;
+  }
 
-.left-top-logo {
-  grid-row: 2 / 3;
-  height: 4.0785rem;
-  width: 15.5573rem;
-}
+  .left-top {
+    display: grid;
+    grid-template-rows: 15% 4.0785rem 15% auto;
+    height: 27.7vh;
+  }
 
-.left-top-content {
-  grid-row: 4 / 5;
-  color: #000;
-  font-size: 0.875rem;
-  font-weight: 300;
-  letter-spacing: 0.05em;
-}
+  .left-top-logo {
+    grid-row: 2 / 3;
+    height: 4.0785rem;
+    width: 15.5573rem;
+  }
 
-.left-top-content > p {
-  margin: 0.5rem 0 0 0;
-}
+  .left-top-content {
+    grid-row: 4 / 5;
+    color: #000;
+    font-size: 0.875rem;
+    font-weight: 300;
+    letter-spacing: 0.05em;
+  }
 
-.right {
-  grid-column: 2 / 3;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 15% auto;
-  grid-column-gap: 6.77vw;
-  height: 27.7vh;
-  width: 40.6vw;
-}
+  .left-top-content > p {
+    margin: 0.5rem 0 0 0;
+  }
 
-.right-obj {
-  grid-row: 2 / 3;
-}
+  .right {
+    grid-column: 2 / 3;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: 15% auto;
+    grid-column-gap: 6.77vw;
+    height: 27.7vh;
+    width: 40.6vw;
+  }
 
-.right-obj-title > p {
-  color: #000;
-  font-size: 1.125rem;
-  font-weight: 500;
-  letter-spacing: 0.05em;
-  text-align: left;
-}
+  .right-obj {
+    grid-row: 2 / 3;
+  }
 
-.right-obj-line {
-  background-color: #000;
-  height: 1px;
-  margin: 1rem 0 1rem 0;
-}
+  .right-obj-title > p {
+    color: #000;
+    font-size: 1.125rem;
+    font-weight: 500;
+    letter-spacing: 0.05em;
+    text-align: left;
+  }
 
-.right-obj-content > p {
-  color: #000;
-  font-size: 0.9375rem;
-  font-weight: 300;
-  letter-spacing: 0.05em;
-  text-align: left;
-  margin: 0.5rem 0 0 0;
-}
+  .right-obj-line {
+    background-color: #000;
+    height: 1px;
+    margin: 1rem 0 1rem 0;
+  }
 
-.right-obj-content-link {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  grid-row-gap: 1rem;
-}
+  .right-obj-content > p {
+    color: #000;
+    font-size: 0.9375rem;
+    font-weight: 300;
+    letter-spacing: 0.05em;
+    text-align: left;
+    margin: 0.5rem 0 0 0;
+  }
 
-.right-obj-content-link-img {
-  height: 2rem;
-  width: 2rem;
+  .right-obj-content-link {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    grid-row-gap: 1rem;
+  }
+
+  .right-obj-content-link-img {
+    height: 2rem;
+    width: 2rem;
+  }
 }
 
 </style>
